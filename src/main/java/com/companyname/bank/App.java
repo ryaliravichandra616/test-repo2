a411db7f902823368782db53902b4facf94c082e
ryaliravichandra616 <ryaliravichandra616@gmail.com>
2025-12-03 10:30:45 +0530

    polling

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "new version/release" );
+        System.out.println( "new version/release for polling " );
     }
 }
+
